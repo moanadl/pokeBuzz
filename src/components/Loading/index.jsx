3853e4762d05a70897faed25657fb9b9dbfc8c2a
Moana <moanadl@gmail.com>
2025-06-13 18:07:13 -0300

    Adding Loading component

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,13 @@
+import './loading.css'
+
+function Loading () {
+
+	return (
+       <div className="loading">
+            <div className="loading-pokeball"></div>
+            <p>Loading...</p>
+        </div>
+    );
+}
+
+export default Loading;
